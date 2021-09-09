602636b871b0174f994ad06412e41603ad2af817
sinyo <sinyomoon@gmail.com>
2021-09-09 23:49:21 +0700

    Fix height

--- pages/[lng]/index.tsx
+++ pages/[lng]/index.tsx
@@ -47,7 +47,7 @@ const Home: FC<any> = ({
             data={dataBanners?.data}
             Carousel={Carousel}
             classes={classesBanner}
-            autoPlay={isReady ? 5000 : null}
+            autoPlay={ null}
             infinite
             dots
             thumborSetting={{
