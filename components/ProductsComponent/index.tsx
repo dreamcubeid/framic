bcdb8a736080594ff055b63cfb590ee0e71a826d
sinyo <sinyomoon@gmail.com>
2021-09-15 20:11:04 +0700

    feat/base-productComponent
    - Product Component
    - Widget Products Home Page

---
+++ components/ProductsComponent/index.tsx
@@ -0,0 +1,69 @@
+/* library package */
+import { FC, useState } from 'react'
+/* component */
+import ProductsWidget from './ProductsWidget'
+import styles from 'public/scss/components/Product.module.scss'
+
+export type ProductsComponentType = {
+  type: "list" | "widget" | "recomendation"
+  i18n: any
+  lng: any
+  tagName?: string
+  itemPerPage?: number
+}
+
+const classesProducts = {
+  productContainerClassName: styles.product_container,
+  productImageContainerClassName: styles.product_imageContainer,
+  productImageClassName: styles.product_imageItem,
+  productLabelContainerClassName: styles.product_labelContainer,
+  productTitleClassName: styles.product_title,
+  productPriceClassName: styles.product_price,
+  // salePriceClassName: styles.product_priceSale,
+  // priceClassName: styles.product_priceText,
+  stickerContainerClassName: styles.product_stickerContainer,
+  outOfStockLabelClassName: styles.product_stickerItemOutOfStock,
+  openOrderLabelClassName: styles.product_stickerItemOpenOrder,
+  comingSoonLabelClassName: styles.product_stickerItemComingSoon,
+  saleLabelClassName: styles.product_stickerItemSale,
+  preOrderLabelClassName: styles.product_stickerItemPreOrder,
+  newLabelClassName: styles.product_stickerItemNew,
+};
+
+const classesPlaceholderProducts = {
+  placeholderList: styles.product_placeholder
+}
+
+const ProductsComponent: FC<ProductsComponentType> = ({
+  type = "list",
+  i18n,
+  tagName,
+  itemPerPage = 4,
+  lng
+}) => {
+  const [totalProducts, settotalProducts] = useState(null)
+
+  if (totalProducts === 0 && type !== "list") return <></>
+
+  return type === "list" ? (
+    <div>
+
+    </div>
+  ) : type === "widget" ? (
+    <ProductsWidget
+      i18n={i18n}
+      lng={lng}
+      tagName={tagName}
+      itemPerPage={itemPerPage}
+      classPlaceholder={classesPlaceholderProducts}
+      classProducts={classesProducts}
+      setTotalProducts={settotalProducts}
+    />
+  ) : type === "recomendation" ? (
+    <div>
+
+    </div>
+  ) : (<></>)
+}
+
+export default ProductsComponent;
--- components/Widget/WidgetHomepageBottom.tsx
+++ components/Widget/WidgetHomepageBottom.tsx
@@ -1,14 +1,17 @@
+/* library package */
 import { FC } from 'react'
 import { Widget } from '@sirclo/nexus'
-import Placeholder from 'components/Placeholder'
 import { LazyLoadComponent } from 'react-lazy-load-image-component'
+/* library template */
 import useWindowSize from 'lib/useWindowSize'
+/* component */
+import Placeholder from 'components/Placeholder'
 
 import styles from 'public/scss/components/WidgetHomePage.module.scss'
 
 const classesWidget = {
   widgetContainer: styles.widget_container,
-  widgetItemBottom: styles.widget_item_bottom,
+  widgetItemBottom: styles.widget_itemBottom,
 };
 
 const classesPlaceholderCollapsibleNav = {
