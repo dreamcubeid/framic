50c9988a6b0d9112a5ae2afed5dff943c9b1aa26
Irsyad Nurilhaq <irsyadnurilhaq@gmail.com>
2021-10-02 17:38:13 +0700

    done contanct page

--- pages/[lng]/contact/index.tsx
+++ pages/[lng]/contact/index.tsx
@@ -1,44 +1,38 @@
-import { FC } from "react";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import dynamic from "next/dynamic";
-import {
-  useI18n,
-  Contact,
-  Widget,
-  isEnquiryAllowed
-} from "@sirclo/nexus";
-import Layout from "components/Layout/Layout";
-import { useBrand } from "lib/useBrand";
-import { toast } from "react-toastify";
-import styles from "public/scss/pages/Contact.module.scss";
+import { FC } from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import dynamic from 'next/dynamic';
+import { useI18n, Contact, Widget, isEnquiryAllowed } from '@sirclo/nexus';
+import Layout from 'components/Layout/Layout';
+import { useBrand } from 'lib/useBrand';
+import { toast } from 'react-toastify';
 
-const Placeholder = dynamic(() => import("components/Placeholder"));
+import styles from 'public/scss/pages/Contact.module.scss';
+import stylesButton from 'public/scss/components/Button.module.scss';
+import stylesForm from 'public/scss/components/Form.module.scss';
+
+const Placeholder = dynamic(() => import('components/Placeholder'));
 
 const classesContact = {
-  containerClassName: `${styles.contact_container} d-flex flex-column align-items-start justify-content-start`,
   mapClassName: styles.contact_map,
-  formContainerClassName: styles.contact_form,
-  titleClassName: "d-none",
-  inputContainerClassName: `${styles.sirclo_form_row}`,
-  inputClassName: `form-control ${styles.sirclo_form_input}`,
-  labelClassName: `d-flex flex-row align-items-center justify-content-start`,
-  buttonContainerClassName: `${styles.contact_buttonContainer} d-block mt-4`,
-  buttonClassName: `${styles.btn} ${styles.btn_primary} ${styles.btn_long} ${styles.btn_full_width} ${styles.btn_center}`,
-  widgetClassName: styles.contact_widget
+  titleClassName: styles.contact_content,
+  inputClassName: stylesForm.form_inputLong,
+  labelClassName: styles.contact_label,
+  buttonContainerClassName: styles.contact_buttonContainer,
+  buttonClassName: `${stylesButton.btn_primaryLong}`,
+  widgetClassName: styles.contact_widget,
 };
 
 const classesPlaceholderContact = {
-  placeholderList: `${styles.placeholderItem} ${styles.placeholderItem_contactWidget}`
-}
+  placeholderList: `${styles.placeholderItem} ${styles.placeholderItem_contactWidget}`,
+};
 
 const ContactPage: FC<any> = ({
   lng,
   lngDict,
-  brand
+  brand,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-
   const i18n: any = useI18n();
-  const allowedEnquiry = isEnquiryAllowed()
+  const allowedEnquiry = isEnquiryAllowed();
 
   return (
     <Layout
@@ -48,14 +42,17 @@ const ContactPage: FC<any> = ({
       brand={brand}
       withAllowed={allowedEnquiry}
     >
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-8 offset-sm2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-
-            <div className={`${styles.contact_info} ${styles.contact_info__top}`}>
-              <h1>{i18n.t("contact.title")}</h1>
+      <div className={styles.contact_container}>
+        <div className={styles.contact_form}>
+          <h4>{i18n.t('contact.title')}</h4>
+          <Contact
+            classes={classesContact}
+            isAddressDetail={false}
+            onCompleted={() => toast.success(i18n.t('contact.submitSuccess'))}
+            onError={() => toast.error(i18n.t('contact.submitError'))}
+            widget={
               <Widget
-                pos="footer-3"
+                pos='footer-4'
                 widgetClassName={styles.contact_info}
                 loadingComponent={
                   <Placeholder
@@ -65,38 +62,19 @@ const ContactPage: FC<any> = ({
                   />
                 }
               />
-            </div>
-
-            <Contact
-              classes={classesContact}
-              isAddressDetail={false}
-              onCompleted={() => toast.success(i18n.t("contact.submitSuccess"))}
-              onError={() => toast.error(i18n.t("contact.submitError"))}
-              widget={
-                <Widget
-                  pos="footer-4"
-                  widgetClassName={styles.contact_info}
-                  loadingComponent={
-                    <Placeholder
-                      classes={classesPlaceholderContact}
-                      withList
-                      listMany={5}
-                    />
-                  }
-                />
-              }
-            />
-          </div>
+            }
+          />
         </div>
       </div>
     </Layout>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const { default: lngDict = {} } = await import(
-    `locales/${params.lng}.json`
-  );
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const { default: lngDict = {} } = await import(`locales/${params.lng}.json`);
 
   const brand = await useBrand(req);
 
@@ -104,9 +82,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     props: {
       lng: params.lng,
       lngDict,
-      brand: brand || ""
-    }
+      brand: brand || '',
+    },
   };
-}
+};
 
 export default ContactPage;
