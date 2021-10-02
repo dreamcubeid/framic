8af3d0fcd78fa2155e721a7509648158d6cd259e
sinyo <sinyomoon@gmail.com>
2021-10-02 12:20:44 +0700

    feat/login-register

--- pages/[lng]/login/index.tsx
+++ pages/[lng]/login/index.tsx
@@ -2,20 +2,20 @@
 import { FC } from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { toast } from 'react-toastify'
-import {
-  Login,
-  SingleSignOn,
-  useI18n
-} from '@sirclo/nexus'
+import { Login, useI18n } from '@sirclo/nexus'
 /* library template */
 import redirectIfAuthenticated from 'lib/redirectIfAuthenticated'
 import { parseCookies } from 'lib/parseCookies'
 import { useBrand } from 'lib/useBrand'
-import { useGoogleAuth } from 'lib/useGoogleAuth'
-import { useFacebookAuth } from 'lib/useFacebookAuth'
+import {
+  useGoogleAuth,
+  useFacebookAuth,
+  useWhatsAppOTPSetting
+} from 'lib/useSingleSignOn'
 /* component */
-import Layout from 'components/Layout/Layout'
 import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
+import Layout from 'components/Layout/Layout'
+import LoginRegisterOTP from 'components/LoginRegisterOTP'
 /* styles */
 import styles from 'public/scss/pages/Login.module.scss'
 import stylesButton from 'public/scss/components/Button.module.scss'
@@ -35,6 +35,7 @@ const LoginPage: FC<any> = ({
   lngDict,
   brand,
   hasGoogleAuth,
+  hasOtp,
   hasFacebookAuth
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const i18n: any = useI18n()
@@ -51,29 +52,25 @@ const LoginPage: FC<any> = ({
       <Breadcrumb links={linksBreadcrumb} lng={lng} />
       <div className={styles.login_wrapper}>
         <div className={styles.login_container}>
-          <h3 className={styles.login_title}>{i18n.t("login.title")}</h3>
-          <Login
-            classes={loginClasses}
-            onCompletedMsg={(msg: string) => toast.success(msg)}
-            onErrorMsg={(msg: string) => toast.error(msg)}
-            passwordViewIcon={<span className={styles.login_viewIcon} />}
-            passwordHideIcon={<span className={styles.login_hideIcon} />}
-            loadingComponent={
-              <p>{i18n.t("global.loading")}</p>
-            }
-          />
-
-          {(hasGoogleAuth || hasFacebookAuth) &&
-            <div className={styles.login_otherLoginContainer}>
-              <label className={styles.login_or}>{i18n.t("login.or")}</label>
-              <SingleSignOn
-                className="login_sso"
-                buttonText={i18n.t("login.sso")}
-                onErrorMsg={(msg: string) => toast.error(msg)}
-                loadingComponent={<></>}
-              />
-            </div>
-          }
+          <LoginRegisterOTP
+            type="login"
+            hasOtp={hasOtp}
+            brand={brand}
+            title={<h3 className={styles.login_title}>{i18n.t("login.title")}</h3>}
+            hasGoogleAuth={hasGoogleAuth}
+            hasFacebookAuth={hasFacebookAuth}
+          >
+            <Login
+              classes={loginClasses}
+              onCompletedMsg={(msg: string) => toast.success(msg)}
+              onErrorMsg={(msg: string) => toast.error(msg)}
+              passwordViewIcon={<span className={styles.login_viewIcon} />}
+              passwordHideIcon={<span className={styles.login_hideIcon} />}
+              loadingComponent={
+                <p>{i18n.t("global.loading")}</p>
+              }
+            />
+          </LoginRegisterOTP>
         </div>
       </div>
     </Layout>
@@ -94,6 +91,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const cookies = parseCookies(req)
   const hasGoogleAuth = await useGoogleAuth(req)
   const hasFacebookAuth = await useFacebookAuth(req)
+  const hasOtp = await useWhatsAppOTPSetting(req)
   redirectIfAuthenticated(res, cookies, 'account')
 
   return {
@@ -102,6 +100,7 @@ export const getServerSideProps: GetServerSideProps = async ({
       lngDict,
       hasGoogleAuth,
       hasFacebookAuth,
+      hasOtp,
       brand: brand || ""
     }
   }
--- pages/[lng]/register/index.tsx
+++ pages/[lng]/register/index.tsx
@@ -8,25 +8,26 @@ import {
   Calendar,
   CheckCircle
 } from 'react-feather'
-import {
-  Register,
-  useI18n,
-  SingleSignOn
-} from '@sirclo/nexus'
+import { Register, useI18n } from '@sirclo/nexus'
 /* library template */
 import redirectIfAuthenticated from 'lib/redirectIfAuthenticated'
 import { parseCookies } from 'lib/parseCookies'
 import { useBrand } from 'lib/useBrand'
-import { useGoogleAuth } from 'lib/useGoogleAuth'
-import { useFacebookAuth } from 'lib/useFacebookAuth'
+import {
+  useGoogleAuth,
+  useFacebookAuth,
+  useWhatsAppOTPSetting
+} from 'lib/useSingleSignOn'
 /* component */
+import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
 import Layout from 'components/Layout/Layout'
 import Loader from 'components/Loader/Loader'
-import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
+import LoginRegisterOTP from 'components/LoginRegisterOTP'
 /* styles */
 import styles from 'public/scss/pages/Register.module.scss'
 import stylesForm from 'public/scss/components/Form.module.scss'
 import stylesButton from 'public/scss/components/Button.module.scss'
+import stylesPassword from 'public/scss/components/PasswordStrength.module.scss'
 
 const classesRegister = {
   headerLabelClassName: styles.register_headerLabel,
@@ -34,22 +35,20 @@ const classesRegister = {
   buttonClassName: stylesButton.btn_primaryLong,
   verificationContainerClassName: styles.register_verificationContainer,
   labelRequiredClassName: stylesForm.form_label,
-  // inputClassName: `form-control ${styles.sirclo_form_input}`,
-  // basicInfoContainerClassName: "d-block m-0 p-0",
-  // containerClassName: `${styles.login_item} ${styles.login_item__form} order-3`,
-  // deliveryAddressContainerClassName: "col-12",
-  // datePickerInputClassName: "date-picker__input",
-  // datePickerCalendarClassName: "date-picker__calendar",
-  // passwordStrengthBarClassName: styles.passwordBar,
-  // passwordStrengthBarContainerClassName: styles.passwordValidation,
-  // passwordCriteriaListClassName: `${styles.formPassword} d-none`,
-  // passwordCriteriaClassName: styles.formPasswordList,
+
+   // Password Field
+   passwordStrengthBarContainerClassName: stylesPassword.passwordStrength,
+   passwordStrengthBarClassName: stylesPassword.passwordStrength_bar,
+   passwordStrengthLabelClassName: stylesPassword.passwordStrength_label,
+   passwordCriteriaClassName: stylesPassword.passwordStrength_criteriaItem,
+   passwordCriteriaListClassName: stylesPassword.passwordStrength_criteria,
 }
 
 const RegisterPage: FC<any> = ({
   lng,
   lngDict,
   brand,
+  hasOtp,
   hasGoogleAuth,
   hasFacebookAuth
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
@@ -69,39 +68,35 @@ const RegisterPage: FC<any> = ({
       <div className={styles.register_wrapper}>
         <div className={styles.register_container}>
           <h3 className={styles.register_title}>{i18n.t("register.title")}</h3>
-          <Register
-            classes={classesRegister}
-            withHeaderLabel={true}
-            onErrorMsg={(msg) => toast.error(msg)}
-            onSuccessMsg={(msg) => toast.success(msg)}
-            redirectPage={() => Router.push(`/[lng]/login`, `/${lng}/login`)}
-            passwordViewIcon={<span className={styles.register_viewIcon} />}
-            passwordHideIcon={<span className={styles.register_hideIcon} />}
-            passwordFulfilledCriteriaIcon={<CheckCircle color="green" size={16} />}
-            passwordUnfulfilledCriteriaIcon={<CheckCircle color="gray" size={16} />}
-            datePickerCalendarIcon={<Calendar />}
-            withVerification={true}
-            isVerified={isVerified}
-            loadingComponent={<Loader color="text-light" />}
-            verificationComponent={
-              <ReCAPTCHA
-                sitekey={process.env.NEXT_PUBLIC_SITEKEY_RECAPTCHA}
-                onChange={() => setIsVerified(true)}
-              />
-            }
-          />
-
-          {(hasGoogleAuth || hasFacebookAuth) &&
-            <div className={styles.login_otherLoginContainer}>
-              <label className={styles.login_or}>{i18n.t("login.or")}</label>
-              <SingleSignOn
-                className="login_sso"
-                buttonText={i18n.t("login.sso")}
-                onErrorMsg={(msg: string) => toast.error(msg)}
-                loadingComponent={<></>}
-              />
-            </div>
-          }
+          <LoginRegisterOTP
+            type="register"
+            hasOtp={hasOtp}
+            brand={brand}
+            hasGoogleAuth={hasGoogleAuth}
+            hasFacebookAuth={hasFacebookAuth}
+          >
+            <Register
+              classes={classesRegister}
+              withHeaderLabel={true}
+              onErrorMsg={(msg) => toast.error(msg)}
+              onSuccessMsg={(msg) => toast.success(msg)}
+              redirectPage={() => Router.push(`/[lng]/login`, `/${lng}/login`)}
+              passwordViewIcon={<span className={styles.register_viewIcon} />}
+              passwordHideIcon={<span className={styles.register_hideIcon} />}
+              passwordFulfilledCriteriaIcon={<CheckCircle color="green" size={16} />}
+              passwordUnfulfilledCriteriaIcon={<CheckCircle color="gray" size={16} />}
+              datePickerCalendarIcon={<Calendar />}
+              withVerification={true}
+              isVerified={isVerified}
+              loadingComponent={<Loader color="text-light" />}
+              verificationComponent={
+                <ReCAPTCHA
+                  sitekey={process.env.NEXT_PUBLIC_SITEKEY_RECAPTCHA}
+                  onChange={() => setIsVerified(true)}
+                />
+              }
+            />
+          </LoginRegisterOTP>
         </div>
       </div>
     </Layout>
@@ -122,6 +117,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const cookies = parseCookies(req)
   const hasGoogleAuth = await useGoogleAuth(req)
   const hasFacebookAuth = await useFacebookAuth(req)
+  const hasOtp = await useWhatsAppOTPSetting(req)
   redirectIfAuthenticated(res, cookies, 'account')
 
   return {
@@ -130,6 +126,7 @@ export const getServerSideProps: GetServerSideProps = async ({
       lngDict,
       hasGoogleAuth,
       hasFacebookAuth,
+      hasOtp,
       brand: brand || ""
     }
   }
