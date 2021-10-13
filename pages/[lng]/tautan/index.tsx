dad58bfd0ad53e0f470b1fe79bdcdcf602cc108b
Irsyad Nurilhaq <irsyadnurilhaq@gmail.com>
2021-10-13 18:50:13 +0700

    wip

--- pages/[lng]/tautan/index.tsx
+++ pages/[lng]/tautan/index.tsx
@@ -32,11 +32,7 @@ const TautanPage: FC<any> = ({
       withFooter={false}
     >
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-8 offset-sm2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-            <Links classes={classesLinks} />
-          </div>
-        </div>
+        <Links classes={classesLinks} />
       </div>
     </Layout>
   )
