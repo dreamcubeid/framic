9b97c82f6661e968bda84711e298f6e705dd460b
Irsyad Nurilhaq <irsyadnurilhaq@gmail.com>
2021-10-13 18:52:12 +0700

    remove fragmet

--- pages/[lng]/payment_notif/[[...orderID]].tsx
+++ pages/[lng]/payment_notif/[[...orderID]].tsx
@@ -1,5 +1,5 @@
 /* library package */
-import { FC, Fragment } from 'react'
+import { FC } from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { useRouter } from 'next/router'
 import { toast } from 'react-toastify'
